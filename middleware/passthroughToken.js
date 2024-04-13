9df9cddedc6a370f14867f2aa965f4aa1145cf19
calteen <pjay32547@gmail.com>
2024-04-13 17:55:49 +0530

    repited chize

--- middleware/passthroughToken.js
+++ middleware/passthroughToken.js
@@ -6,13 +6,12 @@ export const passthroughToken = (req, res, next) => {
   if (token){
     jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
           if (err){
-         req.userId = 0
+            req.userId = 0;
           }else{
             req.userId = payload.id;
           }
-         req.userId = payload.id;
+          
         })
-
   }
 
   next()
